fa217cef867af417ce11ed6154fec7a934f160a2
Rahul Bhardwaj <bhardwaj.rahul@india.com>
2019-01-20 20:46:41 +0530

    Created start.sh file

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+docker-compose down
+
+docker-compose up -d --force-recreate
+
+docker run --rm -v $(pwd):/app composer install
+
+docker-compose exec app php artisan key:generate
+
+docker-compose exec app php artisan config:clear
+
+>&2 echo "Waiting for MySql to start. Please wait....."
+
+WORKING_DIR=$( dirname "${BASH_SOURCE[0]}" )
+BUILD_DIR=$WORKING_DIR/vendor
+if [ -d $BUILD_DIR ]; then
+	sleep 1s
+else
+    >&2 echo "MySql might require some time to start as you are setting up App for first time "
+    sleep 240s
+fi
+
+>&2 echo "MySql started "
+>&2 echo "Running all phpunit tests now...."
+
+docker-compose exec app php artisan migrate
+
+docker run -v $(pwd):/app --rm nielsvdoorn/laravel-phpunit:latest ./vendor/bin/phpunit --configuration phpunit.xml tests
+
+>&2 echo "Your application back end is now ready to serve APIs"
